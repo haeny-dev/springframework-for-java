6ee10e74d1f3b9d0799d8922ebb391b8ef43d7b2
haeny-dev <wjdeoaks92@gmail.com>
2022-01-30 18:04:14 +0900

    [Inflearn] 스프링 입문_2.스프링 웹 개발 기초_API

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,39 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    /**
+     * @ResponseBody
+     * viewResolver 대신에 HttpMessageConverter 가 동작
+     *
+     * 1) 기본 문자 처리: StringHttpMessageConverter
+     * 2) 기본 객체 처리: MappingJackson2HttpMessageConverter
+     * ...byte 처리 등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음.
+     */
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
