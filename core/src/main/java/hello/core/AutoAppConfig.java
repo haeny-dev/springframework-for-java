da3737613d36c16eea042031e43cd3d07f02af19
haeny-dev <wjdeoaks92@gmail.com>
2022-02-08 22:00:02 +0900

    [Inflearn] 스프링 핵심 원리 > 의존관계 자동 주입 > 옵션 처리

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -22,10 +22,11 @@ import org.springframework.context.annotation.FilterType;
 )
 
 public class AutoAppConfig {
-
+/*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
     }
+*/
 
 }
