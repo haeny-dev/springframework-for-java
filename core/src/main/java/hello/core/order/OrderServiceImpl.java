a6f5adc8bc454808add2db36fee6659da1c61c3f
haeny-dev <wjdeoaks92@gmail.com>
2022-02-08 22:57:13 +0900

    [Inflearn] 스프링 핵심 원리 > 의존관계 자동 주입 > 애노테이션 직접 만들기

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
@@ -15,7 +16,7 @@ public class OrderServiceImpl implements OrderService{
      * 생성자 주입
      * - @Autowired 생성자가 딱 하나일 때는 생략 가능
      */
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
