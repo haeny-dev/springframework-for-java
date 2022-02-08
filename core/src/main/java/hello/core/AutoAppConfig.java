5c39ad1973ade012f521deee2ab9caa1ab22439b
haeny-dev <wjdeoaks92@gmail.com>
2022-02-08 20:46:41 +0900

    [Inflearn] 스프링 핵심 원리 > 컴포넌트 스캔 > 탐색 위치와 기본 스캔 대상

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,14 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        /**
+         * default
+         * - @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작위치가 된다.
+         * - 권장하는 방법은 패키지 위치를 지정하지 않고, 설정 정보 클래스의 위치를 프로젝트 최상단에 두는 것이다.
+         * - 스프링부트도 이 방법을 기본으로 제공한다. (@SpringBootApplication)
+         */
+//        basePackages = "hello.core",
+//        basePackageClasses = AutoAppConfig.class,
         // 기존의 AppConfig.class를 제외하기 위해서 설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
