b34dc0d7b9c93f03167bbcc63231f8456187c01b
haeny-dev <wjdeoaks92@gmail.com>
2022-02-08 20:33:12 +0900

    [Inflearn] 스프링 핵심 원리 > 컴포넌트 스캔 > 컴포넌트 스캔과 의존관계 자동 주입 시작하기

---
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // 기존의 AppConfig.class를 제외하기 위해서 설정
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+
+public class AutoAppConfig {
+
+}
