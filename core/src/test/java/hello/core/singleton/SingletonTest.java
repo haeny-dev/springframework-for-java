760b07e046338cefc930e090c31273cf9b092824
haeny-dev <wjdeoaks92@gmail.com>
2022-02-08 17:02:22 +0900

    [Inflearn] 스프링 핵심 원리 > 싱글톤 컨테이너 > 싱글톤 패턴

--- core/src/test/java/hello/core/singleton/SingletonTest.java
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -12,7 +12,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
-    void pureContatiner(){
+    void pureContatiner() {
         AppConfig appConfig = new AppConfig();
 
         // 1.조회: 호출할 때 마다 객체를 생성
@@ -29,4 +29,19 @@ public class SingletonTest {
         assertThat(memberService1).isNotSameAs(memberService2);
     }
 
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+//        new SingletonService()
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same => ==
+        // equal => equals
+    }
 }
