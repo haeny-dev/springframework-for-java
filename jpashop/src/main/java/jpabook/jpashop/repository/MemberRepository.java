89ea548bf1023d318ac24e00a0ad455bedbd2e95
haeny-dev <wjdeoaks92@gmail.com>
2022-02-15 12:05:44 +0900

    [Inflearn] 스프링 부트와 JPA 활용1 > 회원 도메인 개발 > 회원 서비스 개발

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,13 +9,19 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+//    @PersistenceContext
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
+        /**
+         * em.persist 를 하는 순간 영속성 컨텍스트에 해당 멤버를 올린다.
+         * 영속성 컨텍스트에 Key, Value 값으로 들어갈 때 Key 가 id 값이 된다.
+         * DB에 값이 들어간 상태가 아니더라도, id 값이 생성되어 있는게 보장이 된다.
+         */
     }
 
     public Member findOne(Long id){
