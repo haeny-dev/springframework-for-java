ddcc4c946395f0e9829d4a11ed51e934dffce5da
haeny-dev <wjdeoaks92@gmail.com>
2022-02-22 10:38:43 +0900

    [스프링 부트와 JPA 활용2] API 개발 기본 > 회원 조회 API

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +18,49 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /**
-     * 회원등록V1
-     * - 요청 값으로 Member 엔티티를 직접 받는다.
+     * 회원 조회 API V1: 응답 값으로 엔티티를 직접 외부에 노출한다.
+     *
+     * 문제점
+     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+     * - 기본적으로 엔티티의 모든 값이 노출된다.
+     * - 응답 스펙을 맞추기 위해 로직이 추가된다.
+     * - 실무에서는 같은 엔티티에 대해 API 가 용도가 다양하게 만들어지는데,
+     *      한 엔티티에 각각의 API 를 위한 프레젠테이션 응답 로직을 담기는 어렵다.
+     * - 엔티티가 변경되면 API 스펙이 변한다.
+     * - 추가로 컬렉션을 직접 반환하면 향후 API 스펙을 변경하기 어렵다.
+     *
+     * 결론
+     * - API 응답 스펙에 맞추어 별도의 DTO 를 반환한다.
+     * @return
+     */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    /**
+     * 회원 등록 API V1: 요청 값으로 Member 엔티티를 직접 받는다.
      * <p>
      * 문제점
      * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
@@ -36,7 +79,7 @@ public class MemberApiController {
     }
 
     /**
-     * 등록 API V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO 를 받는다.
+     * 회원 등록 API V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO 를 받는다.
      *
      * @param request
      * @return
@@ -52,7 +95,7 @@ public class MemberApiController {
     }
 
     /**
-     * 수정 API
+     * 회원 수정 API V2
      */
     @PutMapping("/api/v2/members/{id}")
     public UpdateMemberResponse updateMemberV2(@PathVariable Long id,
