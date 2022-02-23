f80d9e3594c5b7b89d0f2170a991e1b3405a1a4b
haeny-dev <wjdeoaks92@gmail.com>
2022-02-24 08:54:23 +0900

    [스프링 데이터 JPA] 예제 도메인 모델 > 예제 도메인 모델과 동작확인

---
+++ data-jpa/src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,65 @@
+package study.datajpa.entity;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    void testEntity() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        // when
+        em.persist(teamA);
+        em.persist(teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        List<Team> teams = em.createQuery("select t from Team t", Team.class)
+                .getResultList();
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        // then
+        assertThat(teams.size()).isEqualTo(2);
+        assertThat(teams).contains(teamA, teamB);
+        assertThat(members.size()).isEqualTo(4);
+        assertThat(members).contains(member1, member2, member3, member4);
+
+        // 초기화
+        em.flush();
+        em.clear();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.getTeam() = " + member.getTeam());
+        }
+    }
+
+}
