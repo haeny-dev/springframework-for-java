fdf83e30c038f0af820adccfe77e02445999c3a6
haeny-dev <wjdeoaks92@gmail.com>
2022-02-24 09:13:14 +0900

    [스프링 데이터 JPA] 공통 인터페이스 기능 > 순수 JPA 기반 리포지토리 만들기

--- data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -31,5 +33,25 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);   // JPA 엔티티 동일성 보장
+    }
+
+    @Test
+    void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).orElse(null);
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).orElse(null);
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+        assertThat(memberJpaRepository.findAll().size()).isEqualTo(2);
+        assertThat(memberJpaRepository.count()).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        assertThat(memberJpaRepository.count()).isEqualTo(0);
     }
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -17,6 +17,7 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+
     @Test
     void testMember() {
         // given
