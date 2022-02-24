9b0314463020373f69a83978c02310e477027c78
haeny-dev <wjdeoaks92@gmail.com>
2022-02-24 10:19:08 +0900

    [스프링 데이터 JPA] 쿼리 메소드 기능 > @Query, 리포지토리 메소드에 쿼리 정의하기

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -13,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name = "Member.findByUsername") // 생략가능
     List<Member> findByUsername(@Param("username") String username);
+
+    /* NamedQuery 와 마찬가지로 오타를 내거나 하면 애플리케이션 로딩 시점에 파싱해서 문제가 있으면 오류 발생 */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
