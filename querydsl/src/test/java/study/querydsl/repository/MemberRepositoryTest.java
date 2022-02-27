f1fdc2fe7dbc747adab880083f83c71e63da99ba
haeny-dev <wjdeoaks92@gmail.com>
2022-02-27 14:09:20 +0900

    [Querydsl] 실무 활용 - 스프링 데이터 JPA와 Querydsl > 스프링 데이터 JPA 리포지토리 변경

---
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.querydsl.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void basicTest() {
+        Member member = new Member("member1", 10);
+        memberRepository.save(member);
+
+        memberRepository.findById(member.getId())
+                .ifPresent(m -> assertThat(m).isEqualTo(member));
+
+        assertThat(memberRepository.findAll()).containsExactly(member);
+        assertThat(memberRepository.findByUsername(member.getUsername())).containsExactly(member);
+    }
+
+}
