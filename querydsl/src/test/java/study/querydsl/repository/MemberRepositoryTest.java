91156773c40f63646a406cfe7d07a52e716d6344
haeny-dev <wjdeoaks92@gmail.com>
2022-02-27 14:32:43 +0900

    [Querydsl] 실무 활용 - 스프링 데이터 JPA와 Querydsl > 사용자 정의 리포지토리

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -32,6 +37,35 @@ class MemberRepositoryTest {
 
         assertThat(memberRepository.findAll()).containsExactly(member);
         assertThat(memberRepository.findByUsername(member.getUsername())).containsExactly(member);
+    }
+
+    @Test
+    void searchTest() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        // when
+        List<MemberTeamDto> result = memberRepository.search(condition);
+
+        // then
+        assertThat(result).extracting("username")
+                .containsExactly("member4");
     }
 
 }
