f9cb81a661886f7e6ff28c62322587ce2c8e6762
haeny-dev <wjdeoaks92@gmail.com>
2022-02-27 15:29:37 +0900

    [Querydsl] 스프링 데이터 JPA가 제공하는 Querydsl 기능 > 인터페이스 지원 - QuerydslPredicateExecutor

--- querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ querydsl/src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -18,6 +19,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -98,6 +100,30 @@ class MemberRepositoryTest {
                 .containsExactly("member1", "member2", "member3");
     }
 
+    @Test
+    void querydslPredicateExecutor() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        // when
+        Iterable<Member> result = memberRepository.findAll(
+                member.age.between(10, 40).and(member.username.eq("member1")));
+
+        if (result.iterator().hasNext()) {
+            assertThat(result.iterator().next().getUsername()).isEqualTo("member1");
+        }
+    }
 
 
 }
