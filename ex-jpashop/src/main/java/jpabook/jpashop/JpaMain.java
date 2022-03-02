5ff7225fcfec74b7ddc125345f9ee9945c2c4cc4
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 10:54:03 +0900

    [자바 ORM 표준 JPA] 엔티티 매핑 > 실전 예제 - 요구사항 분석과 기본 매핑, 데이터 중심 설계의 문제점

---
+++ ex-jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpashop");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();    // 트랜잭션 시작
+
+        try {
+
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
