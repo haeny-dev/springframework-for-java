4c23152da8d8c184a27cbc1b5359162e319d29d6
haeny-dev <wjdeoaks92@gmail.com>
2022-03-02 09:50:57 +0900

    [자바 ORM 표준 JPA] 엔티티 매핑 > 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * DDL 생성 기능은 DDL을 자동 생성할 때만 사용되고 JPA의 실행 로직에는 영향을 주지 않는다.
@@ -15,8 +17,34 @@ public class Member {
     private Long id;
 
     // DDL 생성 기능
-    @Column(nullable = false, length = 10)
+    @Column(name = "name",  // 필드와 매핑할 테이블의 컬럼 이름
+            nullable = false, // null 값의 허용 여부 false -> DDL 에 not null 제약조건 추가
+            length = 10 // 길이 제약조건, String 타입에만 사용한다.
+//            insertable = false, updatable = false,  // 등록, 변경 가능 여부
+//            unique = true   // 유니크 제약 조건을 걸때 사용하나 주로 @Table 에서 사용한다.
+//            columnDefinition = "varchar(100) default 'EMPTY'" // 데이터베이스 컬럼 정보를 직접 줄 수 있다.
+    )
     private String name;
+
+    private Integer age;
+
+    // 기본값 EnumType.ORDINAL -> enum 순서를 데이터베이스에 저장하므로 사용X
+    // -> 중간에 enum의 순서가 변경되면 데이터베이스에 데이터가 꼬인다.
+    // EnumType.STRING -> enum 이름을 데이터베이스에 저장
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+//    @Temporal(TemporalType.TIMESTAMP) // LocalDate, LocalDateTime 을 사용할 때는 생략 가능
+    private LocalDateTime createdDate;
+
+//    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime lastModifiedDate;
+
+    @Lob    // 문자열이면 자동으로 CLOB 매핑 나머지는 BLOB
+    private String description;
+
+    @Transient  // 특정 필드를 컬럼에 매핑하지 않음
+    private int temp;
 
     public Long getId() {
         return id;
@@ -32,5 +60,45 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
