950fab2549768d8053b3c03c9a06051883ce5a99
haeny-dev <wjdeoaks92@gmail.com>
2022-03-06 11:23:36 +0900

    [자바 ORM 표준 JPA] 값 타입 > 값 타입의 비교

--- ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/ValueMain.java
@@ -1,14 +1,18 @@
 package hellojpa;
 
+import hellojpa.domain.Address;
+
 public class ValueMain {
     public static void main(String[] args) {
 
         int a = 10;
-        int b = a;
+        int b = 10;
 
-        b = 20;
+        System.out.println("(a == b) = " + (a == b));
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        Address address1 = new Address("seoul", "street", "zipcode");
+        Address address2 = new Address("seoul", "street", "zipcode");
+
+        System.out.println("(address1 == address2) = " + (address1.equals(address2)));
     }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/domain/Address.java
+++ ex1-hello-jpa/src/main/java/hellojpa/domain/Address.java
@@ -1,12 +1,13 @@
 package hellojpa.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter /*@Setter*/
+@EqualsAndHashCode
 public class Address {
     private String city;
     private String street;
@@ -20,4 +21,5 @@ public class Address {
         this.street = street;
         this.zipcode = zipcode;
     }
+
 }
