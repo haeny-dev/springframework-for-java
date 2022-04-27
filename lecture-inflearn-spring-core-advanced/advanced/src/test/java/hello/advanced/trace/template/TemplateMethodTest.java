c93e228b00ddf489794c0c38e1e530bb45cbb89d
haeny-dev <wjdeoaks92@gmail.com>
2022-04-27 12:27:13 +0900

    springCoreAdv: 템플릿 메서드 패턴과 콜백 패턴 > 템플릿 메서드 패턴 예제3

--- lecture-inflearn-spring-core-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ lecture-inflearn-spring-core-advanced/advanced/src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -27,6 +27,29 @@ public class TemplateMethodTest {
         template2.execute();
     }
 
+    /**
+     * 템플릿 메서드 패턴, 익명 내부 클래스 적용
+     */
+    @Test
+    void templateMethodV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+
+        template1.execute();
+        template2.execute();
+    }
+
     private void logic1() {
         long startTime = System.currentTimeMillis();
         // 비즈니스 로직 실행
