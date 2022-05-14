9c762fdbca242ca9f7408497d770a8a92e9996fd
haeny-dev <wjdeoaks92@gmail.com>
2022-05-14 19:21:34 +0900

    springCoreAdv: 스프링 AOP 포인트컷 > 예제만들기

---
+++ lecture-inflearn-spring-core-advanced/aop/src/main/java/hello/aop/member/MemberServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.aop.member;
+
+import hello.aop.member.annotation.ClassAop;
+import hello.aop.member.annotation.MethodAop;
+import org.springframework.stereotype.Component;
+
+@ClassAop
+@Component
+public class MemberServiceImpl implements MemberService{
+
+
+    @MethodAop("test value")
+    @Override
+    public String hello(String param) {
+        return "OK";
+    }
+
+    public String internal(String param) {
+        return "OK";
+    }
+}
