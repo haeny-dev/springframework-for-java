61e5c77f91631386cf1ef74ee1071e48f5db1e84
haeny-dev <wjdeoaks92@gmail.com>
2022-05-18 19:36:08 +0900

    springCoreAdv: 스프링 AOP 실전예제 > 재시도 AOP

--- lecture-inflearn-spring-core-advanced/aop/src/main/java/hello/aop/exam/ExamRepository.java
+++ lecture-inflearn-spring-core-advanced/aop/src/main/java/hello/aop/exam/ExamRepository.java
@@ -1,5 +1,6 @@
 package hello.aop.exam;
 
+import hello.aop.exam.annotation.Retry;
 import hello.aop.exam.annotation.Trace;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,9 @@ public class ExamRepository {
 
     private static int seq = 0;
 
+
     @Trace
+    @Retry(value = 4)
     public String save(String itemId) {
         seq++;
 
